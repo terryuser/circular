ffe61c1701e30fa96e1d17f2a1a6cbf3d5b8d8ab
terryuser <homan03@hotmail.com>
2019-08-20 02:00:02 +0800

    20/8

---
+++ circular_api/js/page/createCircular.js
@@ -0,0 +1,4 @@
+
+var optionHTML = "<div class='selection'><select class='custom-select target'><option selected>Choose...</option><option value='1'>One</option><option value='2'>Two</option><option value='3'>Three</option></select></div>"
+
+$("#selection_group").html();
