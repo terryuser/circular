bdf5dc8143d9c9428095f34489200fab3361a25b
terryuser <homan03@hotmail.com>
2019-08-20 01:06:21 +0800

    19/8 Promise problem

--- circular_api/js/contentEditor.js
+++ circular_api/js/contentEditor.js
@@ -6,18 +6,76 @@ var editor = new EditorJS({
   
     /**
      * Available Tools list.
-     * Pass Tool's class or Settings object for each Tool you want to use
      */
     tools: {
         header: {
           class: Header,
           inlineToolbar : true
         },
-        // ...
+        table: {
+          class: Table,
+          inlineToolbar : true
+        },
+        link: {
+          inlineToolbar : true
+        },
+        quote: {
+          class: Quote,
+          inlineToolbar : true
+        },
+        list: {
+          class: List,
+          inlineToolbar : true
+        },
+        delimiter: {
+          class: Delimiter,
+          inlineToolbar : true
+        }
     },
   
     /**
      * Previously saved data that should be rendered
      */
     data: {}
+  });
+
+
+  var output = {};
+
+  $("#save").click(function() {
+    
+    var target = new Array;
+    $(".target").each(function(){
+      target.push($(this).val());
+    })
+
+    var title = $("#title").val();
+
+    editor.save().then((data) => {
+
+      output = {
+        "target" : target,
+        "title" : title,
+        "content" : data
+      }
+      console.log(output);
+      
+    }).catch((error) => {
+      console.log('Saving failed: ', error)
+    });
+    
+
+    // var content = editor.save().then((contentData) => {
+    //   return contentData;
+    // }).catch((error) => {
+    //   console.log('Saving failed: ', error)
+    // });
+    
+
+    // output = {
+    //   "target" : target,
+    //   "title" : title,
+    //   "content" : contentData
+    // }
+    // console.log(output);
   });
