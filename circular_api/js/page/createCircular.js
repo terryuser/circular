bb5aeebd6cb7bbbad6089005b27213d662bb3c39
terryuser <homan03@hotmail.com>
2019-08-21 02:12:50 +0800

    21/8 input

--- circular_api/js/page/createCircular.js
+++ circular_api/js/page/createCircular.js
@@ -1,8 +1,50 @@
 var version = 1;
+var School_loginID = "demo";
+
+var SchoolID;
+var GroupInfo;
+
+var count = 1;
+
+//Get school
+$.ajax({
+    type: 'POST',
+    url: '/api_v' + version + '/school/' + School_loginID,
+    dataType: "json",
+    async: false,
+    success: function(respon) {
+        SchoolID = respon._id;
+    }
+});
+console.log(SchoolID);
+
+//Get school group
+$.ajax({
+    type: 'POST',
+    url: '/api_v' + version + '/group/' + SchoolID,
+    dataType: "json",
+    async: false,
+    success: function(respon) {
+        GroupInfo = respon.result;
+    }
+});
+console.log(GroupInfo);
+var limit = GroupInfo.length();
 
 
 $(document).ready(function() {
     targetField();
+
+    $("#addOption").click(function(){
+        count++;
+        console.log(count);
+
+        if(count < limit) {
+            targetField();
+        } else {
+            
+        }
+    });
 });
 
 function getData() {
@@ -19,11 +61,13 @@ function getData() {
 }
 
 function targetField() {
-    var count = 1;
-    var selectionOpen = "<div class='selection'><select class='custom-select target' id='target-group-" + count + "'><option selected>Choose...</option>"
-    var option = "<option value='1'>One</option>";
-    var optionHTML = selectionOpen + "<option value='2'>Two</option><option value='3'>Three</option></select></div>"
+    var openHTML = "<div class='selection'><select class='custom-select target' id='target-group-" + count + "'><option selected>All</option>"
+    $("#selection_group").append(openHTML);
 
-    $("#selection_group").append(optionHTML);
+    GroupInfo.forEach(function(option) {
+        $("#target-group-" + count).append("<option value='" + option._id + "'>" + option.name + "</option>");
+    });
+    
+    var closeHTML = "</select></div>";
+    $("#selection_group").append(closeHTML);
 }
-
--- circular_api/js/page/member.js
+++ circular_api/js/page/member.js
@@ -4,6 +4,7 @@ var School_loginID = "demo";
 var SchoolID;
 var GroupInfo;
 
+//Get school
 $.ajax({
     type: 'POST',
     url: '/api_v' + version + '/school/' + School_loginID,
@@ -13,9 +14,9 @@ $.ajax({
         SchoolID = respon._id;
     }
 });
-
 console.log(SchoolID);
 
+//Get school group
 $.ajax({
     type: 'POST',
     url: '/api_v' + version + '/group/' + SchoolID,
@@ -25,17 +26,23 @@ $.ajax({
         GroupInfo = respon.result;
     }
 });
-
 console.log(GroupInfo);
 
 
 $(document).ready(function() {
-    
+    input();
 });
 
 function input() {
+    var count = 1;
+
+    var openHTML = "<div class='selection'><select class='custom-select target' id='target-group-" + count + "'><option selected>All</option>"
+    $("#group_selection").append(openHTML);
+
     GroupInfo.forEach(function(option) {
-        console.log(element);
-      });
-    $("#group_selection").append();
+        $("#target-group-" + count).append("<option value='" + option._id + "'>" + option.name + "</option>");
+    });
+    
+    var closeHTML = "</select></div>";
+    $("#group_selection").append(closeHTML);
 }
